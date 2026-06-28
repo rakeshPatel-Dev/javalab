b3d4509c75453ea5a088b673a56742810fe1a949
Rakesh Patel <rk5080976@gmail.com>
2026-06-28 21:53:48 +0545

    feat(seo): wrap app with HelmetProvider and add base OG meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner';
 import { TooltipProvider } from './components/ui/tooltip.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 
 createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
     <TooltipProvider>
-  <Toaster position="top-right" richColors closeButton />
-    <App />
+      <Toaster position="top-right" richColors closeButton />
+      <App />
     </TooltipProvider>
+  </HelmetProvider>
 )
